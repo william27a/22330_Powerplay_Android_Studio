3ea740e6db5de4e3c256c61f58d2f93d7bf51f20
William Speiser <122130899+wspeiser27@users.noreply.github.com>
2023-03-19 21:15:16 -0700

    movement comments + file

--- 22330_Powerplay_Android_Studio/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/agentControlled/AgentAuto.java
+++ 22330_Powerplay_Android_Studio/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/agentControlled/AgentAuto.java
@@ -1,3 +1,32 @@
 package org.firstinspires.ftc.teamcode.agentControlled;
 
-public class AgentAuto {}
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.File;
+
+@Autonomous(name = "AgentAuto", group = "Agent")
+public class AgentAuto extends LinearOpMode {
+    private File agentFile = new File("org/firstinspires/ftc/teamcode/blank.tflite");
+    Interpreter interpreter = new Interpreter(agentFile);
+
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        telemetry.addLine(interpreter.getSignatureKeys()[0]);
+        telemetry.update();
+        sleep(10000);
+
+        // fl = forward - right + rotation
+        // fr = forward + right - rotation
+        // bl = forward + right + rotation
+        // br = forward - right - rotation
+
+        // forward = (fl + fr) / 2
+        // right = ((fr + bl) / 2) - forward
+        // rotation = fl - forward + right
+    }
+}
