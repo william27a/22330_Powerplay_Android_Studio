4183e4c7831a100131ec8e1596051861e51627c2
William Speiser <122130899+wspeiser27@users.noreply.github.com>
2023-03-23 12:31:25 -0700

    misc

--- 22330_Powerplay_Android_Studio/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAuto.java
+++ 22330_Powerplay_Android_Studio/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAuto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.classes.Arena;
 import org.firstinspires.ftc.teamcode.classes.RobotController;
@@ -15,6 +16,26 @@ public class TestAuto extends LinearOpMode {
         Arena arena = new Arena(hardwareMap, RuntimeType.HARDCODED_AUTO, false);
         RobotController robot = arena.getRobot();
 
+        ElapsedTime time = new ElapsedTime();
+
         waitForStart();
+
+        double startIMU = robot.getRotationDegrees();
+
+        time.reset();
+        robot.chassis.frontLeft.setPower(1);
+        robot.chassis.frontRight.setPower(-1);
+        robot.chassis.backLeft.setPower(1);
+        robot.chassis.backRight.setPower(-1);
+        while (time.seconds() < 1) {}
+        robot.chassis.frontLeft.setPower(0);
+        robot.chassis.frontRight.setPower(0);
+        robot.chassis.backLeft.setPower(0);
+        robot.chassis.backRight.setPower(0);
+
+        telemetry.addData("RPS", Math.abs(startIMU - robot.getRotationDegrees()));
+        telemetry.update();
+
+        sleep(10000);
     }
 }
